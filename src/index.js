d4c7d939b58c844d8075b7c7f48a91d71d697515
Oleh <lemeholeh@gmail.com>
2019-11-25 23:37:23 +0200

    first first push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import{ Fragment } from "react";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+const App = () =>{
+    return(
+        <Fragment>
+        <header>
+            <div className="topText">
+            Hello
+            </div>
+        </header>
+        <main className="topMain">
+            <section className="topSection">
+                <div className="rezText">
+                    <h1>My Name:<span>Oleh Lemekh</span></h1>
+                    <h1>Date of birth:<span>04.04.1991</span></h1>
+                    <h1>Address of residence:<span>St. Bandera 62a (Princess Olga: 1)</span></h1>
+                    <h1>Phone: <span>+380987828946</span></h1>
+                    <h1>Email:<span>lemeholeh@gmail.com</span></h1>
+                    <h1>Education:<span>secondary, junior specialist</span></h1>
+                    <h1>Professional Skills:<span>Computer engineer</span></h1>
+                    <h1>Personal skills:<span>junior specialist</span></h1>
+
+                </div>
+
+            </section>
+        </main>
+        </Fragment>
+    );
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
